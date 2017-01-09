042cc3f08510ec793e229cbe04ff71c11eef5e54
DanielCardonaRojas <d.cardona.rojas@gmail.com>
2017-01-09 17:57:44 -0500

    add a a surrounding dialog

--- MachinesDialog.hs
+++ MachinesDialog.hs
@@ -17,6 +17,7 @@ import qualified Brick.Widgets.List as L
 import qualified Brick.Widgets.Center as C
 import qualified Brick.AttrMap as A
 import qualified Brick.Widgets.Edit as E
+import qualified Brick.Widgets.Dialog as D
 import qualified Brick.Focus as F
 import Brick.Widgets.Core
 import qualified Data.Vector as Vec
@@ -39,15 +40,18 @@ import Brick.Widgets.Core
   , viewport
   )
 
-import Brick.Util (fg, on)
+import Brick.Util (bg, fg, on)
 
 
 data WName = MachineList | SearchTextfield | ReadmeVP deriving (Eq,Ord, Show)
+
+data Action = RunScriptOnAll | RunScript deriving (Show, Eq, Enum)
 
 data AppState = AppState
     { _machineList :: L.List WName String
     , _searchTextfield :: E.Editor String WName
     , _focusedWidget :: F.FocusRing WName
+      , _actionDialog :: D.Dialog Action
     } 
         
 makeLenses ''AppState
@@ -57,21 +61,33 @@ makeLenses ''AppState
 
 drawUI :: AppState -> [Widget WName]
 drawUI st = 
-    [ui]
+    [D.renderDialog d $ C.hCenter $ padAll 1 $ ui]
     where
         l = st ^. machineList
-        searchInput =  F.withFocusRing (st ^. focusedWidget) E.renderEditor (st ^. searchTextfield)
+        d = st ^. actionDialog
+        searchInput =  
+            let searchInput' = F.withFocusRing (st ^. focusedWidget) E.renderEditor (st ^. searchTextfield)
+             in 
+               hLimit 65 $
+               vLimit 5 $
+               --hBox [C.hCenter $ str "Search machines: ", (C.hCenter $ hLimit 30 $ padBottom (Pad 2) $ searchInput')]
+               (str "Search machines: " <+> (hLimit 30 $ padBottom (Pad 2) $ searchInput'))
+
         label = str "Machine: " <+> cur <+> str " of " <+> total
         cur = case l^.(L.listSelectedL) of
                 Nothing -> str "-"
                 Just i -> str (show (i + 1))
+
         total = str $ show $ Vec.length $ l^.(L.listElementsL)
+
         box = B.borderWithLabel label $
-              hLimit 65 $
-              vLimit 5 $
-              L.renderList listDrawElement True l
+            hLimit 65 $
+            vLimit 20 $
+            clickable MachineList $
+            L.renderList listDrawElement True l
 
         explanation = 
+            B.border $ 
             hLimit 65 $
             vLimit 5 $
             viewport ReadmeVP Both $
@@ -79,19 +95,18 @@ drawUI st =
                                    : (str <$> [ "Line " <> show i | i <- [2..25::Int] ])
 
         ui = 
-            C.vCenter $ 
-            clickable MachineList $
-                vBox [ C.hCenter $ hLimit 20 $ vLimit 5 $ padBottom (Pad 2) $ searchInput
+            C.center $ 
+                vBox [ C.hCenter searchInput
                      , C.hCenter box
                      , C.hCenter explanation
                      , str " "
-                     , C.hCenter $ str "Press +/- to add/remove list elements."
+                     , C.hCenter $ str "Dinámica y Desarrollo"
                      , C.hCenter $ str "Press Esc to exit."
                      ]
 
 -- | appEventWithSource generate the record field appHandleEvent  :: s -> e -> EventM n (Next s) needed
 -- to generate the brick app after supplying a source for the machine list (IO [a])
-appEventWithSource :: Show a => (String -> IO [a]) -> AppState -> T.BrickEvent WName e -> T.EventM WName (T.Next (AppState))
+appEventWithSource :: (String -> IO [String]) -> AppState -> T.BrickEvent WName e -> T.EventM WName (T.Next (AppState))
 appEventWithSource datasource st (T.VtyEvent e) =
     let 
         l = st
@@ -100,9 +115,9 @@ appEventWithSource datasource st (T.VtyEvent e) =
 
         V.EvKey V.KEsc [] -> M.halt st
 
-        V.EvKey V.KEnter [] ->
-            M.suspendAndResume $ do
-                (setMachineList st <$> datasource (head search))
+        --V.EvKey V.KEnter [] ->
+            --M.suspendAndResume $ do
+                --(setMachineList st <$> datasource (head search))
                 --putStrLn "Suspended. Please enter something and press enter to resume:"
                 --ws <- words <$> getLine
                 --return (setMachineList st ws)
@@ -115,13 +130,19 @@ appEventWithSource datasource st (T.VtyEvent e) =
 
         V.EvKey k [] ->
             case F.focusGetCurrent (st ^. focusedWidget) of
-               Just SearchTextfield -> T.handleEventLensed st searchTextfield E.handleEditorEvent e >>= M.continue
+               Just SearchTextfield -> 
+                   case k of 
+                        V.KEnter -> M.suspendAndResume (setMachineList st <$> datasource (head search))
+                        _ -> T.handleEventLensed st searchTextfield E.handleEditorEvent e >>= M.continue
+
                Just MachineList -> T.handleEventLensed st machineList L.handleListEvent e >>= M.continue
+
                Just ReadmeVP -> 
                    case k of 
                      V.KDown -> M.vScrollBy (M.viewportScroll ReadmeVP) 1 >> M.continue st
                      V.KUp -> M.vScrollBy (M.viewportScroll ReadmeVP) (-1) >> M.continue st
                      _ -> M.continue st
+
                Nothing -> return st >>= M.continue
 
 appEventWithSource source st _ = M.continue st 
@@ -141,18 +162,26 @@ listDrawElement sel a =
         (selStr $ show a)
 
 
-setMachineList :: Show a =>  AppState -> [a] -> AppState
-setMachineList st ls = st &  machineList .~ listMachine where listMachine = L.list MachineList (Vec.fromList $ map show ls) 1
+setMachineList ::  AppState -> [String] -> AppState
+setMachineList st ls = st &  machineList .~ listMachine where listMachine = L.list MachineList (Vec.fromList  ls) 1
 
-initialStateFromList :: Show a => [a] -> AppState
+initialStateFromList :: [String] -> AppState
 initialStateFromList ls = setMachineList defaultState ls
+
+defaultActionDialog :: D.Dialog Action
+defaultActionDialog = D.dialog (Just "Dyd Remote Executor") (Just (0, choices)) 70
+    where
+                choices = [ ("Run on all", RunScriptOnAll)
+                          , ("Run", RunScript)
+                          ]
 
 defaultState :: AppState
 defaultState = 
     AppState 
     { _machineList = L.list MachineList (Vec.fromList []) 1
     , _searchTextfield = (E.editor SearchTextfield (str . unlines) (Just 1) "")
-      , _focusedWidget = F.focusRing [SearchTextfield, MachineList, ReadmeVP]
+    , _focusedWidget = F.focusRing [SearchTextfield, MachineList, ReadmeVP]
+    , _actionDialog = defaultActionDialog
     }
 
 customAttr :: A.AttrName
@@ -162,8 +191,10 @@ theMap :: A.AttrMap
 theMap = A.attrMap V.defAttr
     [ (L.listAttr,            V.white `on` V.blue)
     , (L.listSelectedAttr,    V.blue `on` V.white)
-    --, (L.listSelectedFocusedAttr,    V.black `on` V.yellow)
-    , (B.vBorderAttr,            V.white `on` V.green)
+    --, (D.dialogAttr, V.white `on` V.blue)
+    , (D.buttonAttr, V.black `on` V.white)
+    , (D.buttonSelectedAttr, bg V.magenta)
+    --, (B.vBorderAttr,            V.white `on` V.green)
     , (E.editAttr,            V.white `on` V.blue)
     , (E.editFocusedAttr,     V.black `on` V.yellow)
     , (customAttr,            fg V.cyan)
@@ -184,12 +215,12 @@ customApp source = M.App { M.appDraw = drawUI
                       , M.appStartEvent = return
                       , M.appAttrMap = const theMap}
 
-renderDialog :: Show a => [a] -> (AppState -> IO ()) -> IO ()
+renderDialog :: [String] -> (AppState -> IO ()) -> IO ()
 renderDialog l handler = do
     finalState <-  M.defaultMain theApp (initialStateFromList l)
     handler finalState
 
-runRemoteExecutor :: Show a => (String -> IO [a]) -> (AppState -> IO ()) -> IO ()
+runRemoteExecutor :: (String -> IO [String]) -> (AppState -> IO ()) -> IO ()
 runRemoteExecutor source handler = do
     finalState <-  M.defaultMain (customApp source) defaultState
     handler finalState
