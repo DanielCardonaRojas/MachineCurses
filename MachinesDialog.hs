54d7dc8ff7b446f32d6b87b2dc39920339a33752
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2017-01-19 16:10:31 -0500

    adding information tabs/toggles

--- MachinesDialog.hs
+++ MachinesDialog.hs
@@ -10,7 +10,7 @@ import Control.Monad (void)
 import Data.Monoid
 import qualified Graphics.Vty as V
 import Data.Maybe (fromMaybe)
-import Data.List (intersperse)
+import Data.List (intersperse, elemIndex)
 import Control.Monad.Trans
 
 import qualified Data.Text.Zipper as TZ
@@ -55,13 +55,26 @@ import Brick.Widgets.Core
 
 import Brick.Util (bg, fg, on)
 
+{-
+TODO: 
+    - Move page rendering functions to separete modules.
+
+    - Make the generalize the machine list to a list of tuples and select which information to see 
+      maybe with toggle boxes below the searchbar.
+
+    - Add keydingins to navigate with jk (move up and down the list) lh move selected info toggle and space to toggle selection.
+
+-}
+
 -- | Widget name identifiers
-data WName = MachineList | SearchTextfield | CommandTextfield | ReadmeVP | SideBarOptions | Button Page | NoWidget 
+data WName = MachineList | SearchTextfield | CommandTextfield | ReadmeVP | SideBarOptions | Button Page | Toggles | Toggle InfoToggle | NoWidget 
     deriving (Eq, Ord)
 
 deriving instance Show WName
 
 data Page = PageCustom | PageInfo | PageDiagnostics | PageConfirmation deriving (Eq,Ord, Enum, Bounded)
+
+data InfoToggle = InfoVPN | InfoHost deriving (Show, Enum, Bounded, Ord, Eq)
 
 data Action = OK | Cancel | Quit | RunScriptOnAll | RunScript deriving (Show, Eq, Enum)
 
@@ -72,8 +85,8 @@ data AppState = AppState
     , _searchTextfield :: E.Editor String WName
     , _commandTextfield :: E.Editor String WName
     , _focusedWidget :: F.FocusRing WName
-    , _actionDialog :: D.Dialog Action
-    , _sideBarOptionList :: L.List WName String
+    , _pageDialog :: D.Dialog Action
+    , _infoToggles :: [(InfoToggle, Bool)]
     } 
         
 makeLenses ''AppState
@@ -92,9 +105,7 @@ drawUI :: AppState -> [Widget WName]
 drawUI st = 
     let
         fw = F.focusGetCurrent (st ^. focusedWidget)
-        l = st ^. machineList
-        optionList = st ^. sideBarOptionList
-        d = st ^. actionDialog
+        d = st ^. pageDialog
         ds = D.dialogSelection d
         focusedWidgetIs n =  fmap (== n) fw ?? False
         searchString = mconcat $ E.getEditContents (st ^. searchTextfield)
@@ -110,14 +121,18 @@ drawUI st =
                 allPages = [PageCustom .. (pred PageConfirmation)]
                 buttons = map mkButton $ zip3 (Button <$> allPages) (show <$> allPages) ((fromString . ("button" ++) . show) <$> [1..]) 
 
-
         explanation = 
             padding 6 0 3 2 $
             hLimit 40 $
             vLimit 5 $
             viewport ReadmeVP Vertical $
             clickable ReadmeVP $
-                vBox $ (str <$> [commandString ++ " " ++ searchString, show fw , show ds, show $ getSelectedMachine st, aboutText])
+                vBox $ (str <$> [commandString ++ " " ++ searchString, show fw 
+                                , show ds
+                                , show $ getSelectedMachine st
+                                , show $ st ^. (pageDialog . D.dialogButtonsL) 
+                                , show $ st ^. currentPage
+                                , aboutText])
 
         sideBar = 
             withDefAttr "sidebar" $
@@ -189,6 +204,15 @@ withMachineSearch wid st =
                vLimit 5 $
                (str "Buscar : " <+> (clickable SearchTextfield $ hLimit 30 $ padBottom (Pad 2) $ searchInput'))
 
+        
+        toggles  = 
+            --highlightBorder MachineList $
+            withDefAttr "toggles" $
+            vLimit 8 $
+            hLimit 65 $
+            clickable Toggles $
+            renderHorizontalList listDrawToggles (st ^. infoToggles)
+
         mList = 
             --highlightBorder MachineList $
             withDefAttr "list" $
@@ -199,12 +223,6 @@ withMachineSearch wid st =
             --L.renderList listDrawElement (focusedWidgetIs MachineList) l
             L.renderList listDrawElement True (st ^. machineList)
 
-        commandInput =  
-            let commandinput = F.withFocusRing (st ^. focusedWidget) E.renderEditor (st ^. commandTextfield)
-             in 
-               vLimit 10 $
-               (str "Enter a shell command" <=> (clickable CommandTextfield $  padBottom (Pad 2) $ commandinput))
-
         label = str "Maquina: " <+> cur <+> str " de " <+> total
         cur = case st ^. (machineList . L.listSelectedL) of
                 Nothing -> str "-"
@@ -214,6 +232,7 @@ withMachineSearch wid st =
     in
         mkPage machinePageValidator st $
         vBox [ C.hCenter searchInput
+             , C.hCenter toggles
              , C.hCenter mList
              , str " "
              , wid
@@ -228,9 +247,16 @@ withMachineSearch wid st =
 
 appEventWithSource :: (String -> IO [String]) -> AppState -> T.BrickEvent WName e -> T.EventM WName (T.Next (AppState))
 
+appEventWithSource source st (T.MouseUp n _ loc) = 
+    case n of
+      Button pg ->  M.continue (updateDialogButtons st & currentPage .~ pg)
+      Toggle t ->  M.continue st
+      _ -> M.continue (st & focusedWidget %~ moveFocus n)
+
 appEventWithSource source st (T.MouseDown n _ _ loc) = 
     case n of
       Button pg ->  M.continue (updateDialogButtons st & currentPage .~ pg)
+      Toggle t ->  M.continue (toggleSearchColumnAt t st )
       _ -> M.continue (st & focusedWidget %~ moveFocus n)
 
 appEventWithSource source st (T.MouseUp _ _ _) = M.continue $ st
@@ -239,8 +265,8 @@ appEventWithSource datasource st (T.VtyEvent e) =
     let 
         fw = F.focusGetCurrent (st ^. focusedWidget)
         selectedMachine = getSelectedMachine st
-        --d = st ^. actionDialog
-        ds = D.dialogSelection (st ^. actionDialog)
+        --d = st ^. pageDialog
+        ds = D.dialogSelection (st ^. pageDialog)
         search = E.getEditContents (st ^. searchTextfield)
     in case e of
 
@@ -259,10 +285,10 @@ appEventWithSource datasource st (T.VtyEvent e) =
                         (_, Nothing) -> M.continue st
 
         V.EvKey V.KLeft [] -> 
-            T.handleEventLensed st actionDialog D.handleDialogEvent (V.EvKey V.KLeft []) >>= M.continue
+            T.handleEventLensed st pageDialog D.handleDialogEvent (V.EvKey V.KLeft []) >>= M.continue
 
         V.EvKey V.KRight [] -> 
-            T.handleEventLensed st actionDialog D.handleDialogEvent (V.EvKey V.KRight []) >>= M.continue
+            T.handleEventLensed st pageDialog D.handleDialogEvent (V.EvKey V.KRight []) >>= M.continue
 
         V.EvKey (V.KChar '\t') [] -> M.continue $ st & focusedWidget %~ F.focusNext
 
@@ -273,7 +299,6 @@ appEventWithSource datasource st (T.VtyEvent e) =
                Just SearchTextfield -> T.handleEventLensed st searchTextfield E.handleEditorEvent e >>= M.continue
                Just CommandTextfield -> T.handleEventLensed st commandTextfield E.handleEditorEvent e >>= M.continue
                Just MachineList -> T.handleEventLensed st machineList L.handleListEvent e >>= M.continue
-               Just SideBarOptions -> T.handleEventLensed st sideBarOptionList L.handleListEvent e >>= M.continue
                Just ReadmeVP -> 
                    case k of 
                      V.KDown -> M.vScrollBy (M.viewportScroll ReadmeVP) 1 >> M.continue st
@@ -281,7 +306,7 @@ appEventWithSource datasource st (T.VtyEvent e) =
                      _ -> M.continue st
 
                Nothing -> M.continue st
-                    --T.handleEventLensed st actionDialog D.handleDialogEvent (V.EvKey k []) >>= M.continue
+                    --T.handleEventLensed st pageDialog D.handleDialogEvent (V.EvKey k []) >>= M.continue
 
         _ -> M.continue st
 
@@ -301,8 +326,9 @@ defaultActionDialog =
     defaultDialog & D.dialogButtonsL .~ choices
     where
         defaultDialog = D.dialog (Just "Page Title") Nothing 70
-        choices = [ ("Run on all", RunScriptOnAll)
-                  , ("Run", RunScript)
+        choices = [ 
+                    ("Execute", RunScript)
+                  , ("Execute on All", RunScriptOnAll)
                   ]
 
 focusWidgetList = [SearchTextfield, MachineList, CommandTextfield, SideBarOptions, ReadmeVP]
@@ -316,8 +342,8 @@ defaultState =
     , _searchTextfield = (E.editor SearchTextfield (str . unlines) (Just 1) "")
     , _commandTextfield = (E.editor CommandTextfield (str . unlines) (Just 5) "")
     , _focusedWidget = F.focusRing focusWidgetList
-    , _actionDialog = defaultActionDialog
-    , _sideBarOptionList = L.list SideBarOptions (Vec.fromList ["Run script","Search Machines"]) 1
+    , _pageDialog = defaultActionDialog
+    , _infoToggles = [(InfoVPN, True),(InfoHost, False)]
     }
 
 
@@ -391,13 +417,26 @@ runRemoteExecutor source handler = do
     finalState <-  M.customMain buildVty Nothing (customApp source) defaultState
     handler finalState
 
-runRemoteExecutor' :: (String -> IO [String]) -> (AppState -> IO ()) -> IO ()
-runRemoteExecutor' source handler = do
-    finalState <-  M.defaultMain (customApp source) defaultState
-    handler finalState
-
 
 --------- HELPERS ----------
+
+listDrawToggles :: (InfoToggle, Bool) -> Widget WName
+listDrawToggles (t,b) = 
+    case b of
+        True -> clickable (Toggle t) $
+                (B.border (str " x ")) <+> (str $ show t)
+
+        False -> clickable (Toggle t) $
+                (B.border (str "   ")) <+> (str $ show t)
+
+renderHorizontalList f = horizontalJoin . map f
+    where horizontalJoin = foldr1 (<+>) . intersperse (str " ")
+
+toggleSearchColumnAt :: InfoToggle -> AppState -> AppState
+toggleSearchColumnAt i st = st & (infoToggles . ix m . _2) %~ toggle
+    where 
+        toggle = not
+        m = elemIndex i (map fst (st ^. infoToggles)) ?? 0
 
 listDrawElement :: (Show a) => Bool -> a -> Widget WName
 listDrawElement sel a =
@@ -428,12 +467,13 @@ mkButton (name, label, attr) =
 -- | Add top right bottom and left padding values to a widget at once
 padding t r b l w = padTop (Pad t) $ padBottom (Pad b) $ padLeft (Pad l) $ padRight (Pad r) $ w
 
+updateDialogButtons :: AppState -> AppState
 updateDialogButtons st = 
     let
-        setDialogButtons' l selected = st & (actionDialog . D.dialogButtonsL) .~ l  & (actionDialog . D.dialogSelectedIndexL) .~ selected
+        setDialogButtons' l selected = st & (pageDialog . D.dialogButtonsL) .~ l  & (pageDialog . D.dialogSelectedIndexL) .~ selected
     in
         case st ^. currentPage of
-            PageCustom -> setDialogButtons' [("Execute", RunScript),("Execute on all", RunScriptOnAll)] Nothing
+            PageCustom -> setDialogButtons' [("Execute", RunScript),("Execute on All", RunScriptOnAll)] Nothing
             PageConfirmation -> setDialogButtons' [("OK", OK),("Cancel", Cancel)] Nothing
             PageInfo -> setDialogButtons' [("OK", OK),("Cancel", Cancel)] (Just 1)
             _ -> setDialogButtons' [("OK", OK),("Cancel", Cancel), ("Quit", Quit)] (Just 1)
@@ -442,7 +482,7 @@ mkPage :: Maybe (AppState -> String) -> AppState -> Widget WName -> Widget WName
 mkPage validation st w = 
     let 
         pg = st ^. currentPage
-        d = st ^. actionDialog
+        d = st ^. pageDialog
         style s = C.center $ padBottom (Pad 3) $ s
         w' = maybe w (\v -> style $ str (v st) <=> str " " <=> w) validation
         --w' = maybe w (\v -> str (v st) <=> w) validation
